f0c6fb445fae5c2d950ae7b79ea10de69150685c
David Mohr <david.mohr@ardexa.com>
2018-09-25 13:45:25 +1000

    fix: convert a signed low word to unsigned

--- ardexaplugin.py
+++ ardexaplugin.py
@@ -153,7 +153,11 @@ def convert_int32(high_word, low_word):
 def convert_words_to_uint(high_word, low_word):
     """Convert two words to a floating point"""
     try:
-        number = (int(high_word) << 16) | int(low_word)
+        low_num = int(low_word)
+        # low_word might arrive as a signed number. Convert to unsigned
+        if low_num < 0:
+            low_num = abs(low_num) + 2**15
+        number = (int(high_word) << 16) | low_num
         return number, True
     except:
         return 0, False
