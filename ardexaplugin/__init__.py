645c42db61172221ae133421ac2d20fddac4eb62
David Mohr <david.mohr@ardexa.com>
2020-02-14 11:55:30 +1100

    wip: first cut of dynamic capture support
    * Use more lists
    * add Datetime
    * always log to ARCHIVE and LATEST
    * build header from objects
    * use proper CSV writer

--- ardexaplugin/__init__.py
+++ ardexaplugin/__init__.py
@@ -6,82 +6,156 @@ ardexaplugin
 :license: MIT, see LICENSE for more details.
 """
 
-import os
-import time
+import csv
 import struct
+import time
+import os
+from io import StringIO
 from subprocess import Popen, PIPE
 
-def write_log(log_directory, log_filename, header, logline, debug,
-              require_latest, latest_directory, latest_filename):
-    """This function logs a line of data to both a 'log' file, and a 'latest'
-    file The 'latest' file is optional, and is sent to this function as a
-    boolean value via the variable 'require_latest'.
-    So the 2 log directories and filenames are:
-            a. (REQUIRED): log_directory + log_filename
-            b. (OPTIONAL): latest_directory + latest_filename
+DEBUG = 0
+LATEST_FILENAME = "latest.csv"
+IS_RUNNING = True
+SERVICE_MODE = False
+SERVICE_CACHE = {}
 
-    The 'latest' directory and filename is provided so as to have a consistent
-    file of the latest events This is usually the latest day of events.
-    The way this function works with the 'latest' log_dir is as follows:
-            a. It checks for the existance of log_directory + log_filename
-            b. If (a) doesn't exist, then any 'latest' file is removed and a new one created
-            c. If (a) already exists, logs are written to any existing 'latest' file
-                    If one doesn't exist, it will be created
+def set_debug(debug):
+    """Set the debug level across the whole module"""
+    # pylint: disable=W0603
+    global DEBUG
+    DEBUG = debug
 
-    For both the 'log' and 'latest' files, a header line will be written if a new
-    file is created Please note that a header must start with the '#' symbol, so
-    the Ardexa agent can interpret this line as a header , and will not send it to
-    the cloud
-    """
-    create_new_file = False
 
-    # Make sure the logging directory exists. The following will create all the necessary subdirs,
-    # if the subdirs exist in part or in full
-    if not os.path.exists(log_directory):
+def get_ardexa_field_type(field_type):
+    """Normalise the type"""
+    # pylint: disable=R0911
+    field_type = field_type.lower()
+    if field_type in ("int", "integer"):
+        return "integer"
+    if field_type in ("float", "decimal", "real", "freal", "double"):
+        return "decimal"
+    if field_type in ("bool", "boolean"):
+        return "bool"
+    if field_type == "date":
+        return "date"
+    if field_type == "discard":
+        return "discard"
+    if field_type in ("string", "keyword"):
+        return "keyword"
+    return None
+
+
+def process_header_field(field):
+    """Convert an object to an Ardexa header"""
+    if isinstance(field, str):
+        return field
+
+    if not isinstance(field, dict):
+        raise ValueError("Unknown header value")
+
+    ardexa_type = get_ardexa_field_type(field['type'])
+    if ardexa_type is None:
+        raise ValueError("Unknown field type")
+
+    if 'units' in field and field['units'] is not None:
+        return "{}({}:{})".format(field['name'], ardexa_type, field['units'])
+    return "{}({})".format(field['name'], ardexa_type)
+
+
+def clean_and_stringify_value(value):
+    """This function will escape backslashes since csv.writer does not"""
+    value = str(value)
+    if value.find("\n") != -1:
+        value = value.replace("\n", " ")
+    if value.find("\\") != -1:
+        value = value.replace("\\", r"\\")
+    return value
+
+
+def make_csv_str(data):
+    """Convert list to CSV"""
+    output = StringIO()
+    csvwriter = csv.writer(output, doublequote=False, escapechar='\\', lineterminator='\n')
+    csvwriter.writerow(data)
+    data_line = output.getvalue()
+    output.close()
+    return data_line
+
+
+def get_log_directory(output_directory, table, source):
+    """Generate final log directory.
+
+    Based on the table(string) and source(list). Create the target directory
+    if it doesn't exist"""
+    log_directory = os.path.join(output_directory, table, *[str(s) for s in source])
+    try:
         os.makedirs(log_directory)
-    full_path_log = os.path.join(log_directory, log_filename)
-    if debug > 1:
-        print("Full path of log directory: {}".format(full_path_log))
-    # If the file doesn't exist, annotate that a new 'latest' file is to be created
-    # and that a header is to be created
-    if not os.path.isfile(full_path_log):
-        if debug > 1:
-            print("Log file doesn't exist: {}".format(full_path_log))
-        create_new_file = True
+    except FileExistsError:
+        pass
+    return log_directory
 
-    # Repeat for the 'latest', if it doesn't exist
-    if require_latest:
-        if not os.path.exists(latest_directory):
-            os.makedirs(latest_directory)
-        full_path_latest = os.path.join(latest_directory, latest_filename)
-        if debug > 1:
-            print("Full path of latest directory: {}".format(full_path_latest))
-        # If the 'create_new_file' tag is set AND the file exists, then remove it
-        if create_new_file and os.path.isfile(full_path_latest):
-            # then remove the file
-            os.remove(full_path_latest)
 
-    # Now create both (or open both) and write to them
-    if debug > 1:
-        print("##########################################")
-        print("Writing the line to: {}".format(full_path_latest))
-        print(logline)
-        print("##########################################")
+def get_output_files(log_directory):
+    """Generate absolute file paths for archive and latest"""
+    archive_file_name = "{}.csv".format(time.strftime("%Y-%m-%d"))
 
-    # Write the logline to the log file
-    output_file = open(full_path_log, "a")
-    if create_new_file:
-        output_file.write(header)
-    output_file.write(logline)
-    output_file.close()
+    latest_file = os.path.join(log_directory, LATEST_FILENAME)
+    archive_file = os.path.join(log_directory, archive_file_name)
 
-    # And write it to the 'latest' if required
-    if require_latest:
-        write_latest = open(full_path_latest, "a")
-        if create_new_file:
-            write_latest.write(header)
-        write_latest.write(logline)
-        write_latest.close()
+    return latest_file, archive_file
+
+
+def write_dyn_log(output_directory, table, source, output, changes_only=False):
+    """Turn output into header and data CSV. Append Datetime"""
+    header_line = "# Datetime(date)," + make_csv_str([process_header_field(f[0]) for f in output])
+    data_line = get_datetime_str() + "," + \
+            make_csv_str([clean_and_stringify_value(f[1]) for f in output])
+    log_directory = get_log_directory(output_directory, table, source)
+    write_log(log_directory, header_line, data_line)
+
+
+def write_log(log_directory, header_line, data_line):
+    """Write the data to the files.
+
+    The ARCHIVE file is the historical log. Files are named according to the
+    date.
+
+    The LATEST file is provided so that the Ardexa Agent has a constant point
+    of reference for latest events. LATEST is rotated daily.
+
+    Header line is only written to new files.
+    """
+    write_header = False
+    latest_file, archive_file = get_output_files(log_directory)
+
+    if DEBUG > 1:
+        print("LATEST : {}".format(latest_file))
+        print("ARCHIVE: {}".format(archive_file))
+        print("HEADER : {}".format(header_line), end='')
+        print("DATA   : {}".format(data_line), end='')
+
+    # If the archive file doesn't exist, means we've moved to a new day.
+    # Rotate latest
+    if not os.path.isfile(archive_file):
+        write_header = True
+        if DEBUG > 1:
+            print(" * ARCHIVE doesn't exist, rotating LATEST")
+        try:
+            os.remove(latest_file)
+        except FileNotFoundError:
+            pass
+
+    # Write the data_line to the log file
+    with open(archive_file, "a") as output_archive:
+        if write_header:
+            output_archive.write(header_line)
+        output_archive.write(data_line)
+
+    # And write it to the 'latest'
+    with open(latest_file, "a") as output_latest:
+        if write_header:
+            output_latest.write(header_line)
+        output_latest.write(data_line)
 
 
 def check_pidfile(pidfile, debug):
